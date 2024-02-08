c63fd0b37af2dffc1a28db7f4c6bc2ba9ddd641f
James Bristow <james.bristow@plantandfood.co.nz>
2024-02-09 02:24:31 +1300

    Adding Unit test scaffold

---
+++ SVSModel.Tests/SVSModelTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using System;
+
+namespace SVSModel.Tests
+{
+    public class SVSModelTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
