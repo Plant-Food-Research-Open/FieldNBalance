a89598a7567b4732c08f66fdfebd150034499fd9
Timothy Miller <tim@themillerfamily.co.nz>
2024-03-22 10:28:28 +1300

    feat: comments for this class

--- SVSModel/Configuration/Config.cs
+++ SVSModel/Configuration/Config.cs
@@ -10,15 +10,21 @@ namespace SVSModel.Configuration
         public CropConfig Prior { get; set; }
         public CropConfig Current { get; set; }
         public CropConfig Following { get; set; }
-
-        public readonly List<CropConfig> Rotation = [];
-
+        public List<CropConfig> Rotation { get; set; } = [];
         public FieldConfig Field { get; set; }
 
+        /// <summary>
+        /// Constructor used only by external webapp
+        /// </summary>
         public Config() { }
 
+        /// <summary>
+        /// Constructor used only by the Excel model
+        /// </summary>
         public Config(Dictionary<string, object> c)
         {
+            // Only raw input values should be set in here
+            
             Prior = new CropConfig(c, "Prior");
             Current = new CropConfig(c, "Current");
             Following = new CropConfig(c, "Following");
