43af05ddb52ee0a3839803e31d905e9edff9ff78
Hamish Brown <hamish.brown@plantandfood.co.nz>
2024-06-04 20:58:23 +1200

    Change excel interface so met data is obtained from source met files rather than passed in from excel

--- SVSModel.Excel/ExcelInterface.cs
+++ SVSModel.Excel/ExcelInterface.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using SVSModel.Models;
 using static SVSModel.Configuration.Constants;
 using static SVSModel.Configuration.InputCategories;
+using System.Runtime.InteropServices.ComTypes;
+using SVSModel;
 
 namespace SVSModel.Excel
 {
@@ -63,31 +65,23 @@ namespace SVSModel.Excel
         /// <param name="testResults">2D array with dates in teh first column and soil N test results in the second</param>
         /// <returns>Dictionary with parameter names as keys and parameter values as values</returns>
         [ExcelFunction(Description = "Returns full N balance results")]
-        public static object[,] GetDailyNBalance(object[,] met, object[,] config, object[,] testResults, object[,] nApplied)
+        public static object[,] GetDailyNBalance(object[,] config, object[,] testResults, object[,] nApplied)
         {
             List<string> configErrors = Functions.ValidateConfig(config);
 
-            for (var r = 0; r < met.GetLength(0); r++)
+                if (configErrors.Count == 0)
             {
-                for (var c = 0; c < met.GetLength(1); c++)
-                {
-                    if (met[r, c].GetType() == typeof(ExcelDna.Integration.ExcelEmpty) ||
-                        met[r, c].GetType() == typeof(ExcelDna.Integration.ExcelError))
-                        met[r, c] = null;
-                }
-            }
-
-
-            if (configErrors.Count == 0)
-            {
-                Dictionary<DateTime, double> _tt = Functions.dictMaker(met, "MeanT");
-                Dictionary<DateTime, double> _rain = Functions.dictMaker(met, "Rain");
-                Dictionary<DateTime, double> _pet = Functions.dictMaker(met, "MeanPET");
-                Dictionary<DateTime, double> _testResults = Functions.dictMaker(testResults, "Value");
-                Dictionary<DateTime, double> _nApplied = Functions.dictMaker(nApplied, "Amount");
                 var _config = new Config(Functions.dictMaker(config));
 
-                return Simulation.Simulation.SimulateField(_tt, _rain, _pet, _testResults, _nApplied, _config, Constants.InitialN);
+                var startDate = _config.Prior.EstablishDate.AddDays(-1);
+                var endDate = _config.Following.HarvestDate.AddDays(2);
+                var weatherStation = _config.Field.WeatherStation;
+                MetDataDictionaries metData = ModelInterface.BuildMetDataDictionaries(startDate, endDate, weatherStation, false);
+
+                Dictionary<DateTime, double> _testResults = Functions.dictMaker(testResults, "Value");
+                Dictionary<DateTime, double> _nApplied = Functions.dictMaker(nApplied, "Amount");
+
+                return Simulation.Simulation.SimulateField(metData.MeanT, metData.Rain, metData.MeanPET, _testResults, _nApplied, _config, Constants.InitialN);
             }
             else
             {
