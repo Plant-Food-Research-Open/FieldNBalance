9677c75ef8a1145ecf11bfd6d3f603946ad6a290
Hamish Brown <hamish.brown@plantandfood.co.nz>
2024-06-06 09:19:46 +1200

    Pair report.py with note book and give environment specific root variables so can test report generation locally

--- TestGraphs/report.py
+++ TestGraphs/report.py
@@ -1,16 +1,47 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import os
 import aspose.words as aw
 import os.path as osp
 from glob import glob
 from pathlib import Path
 
 def main():
-    img_path = osp.join("TestGraphs", "Outputs")
-    if not osp.isdir(img_path):
-        raise FileNotFoundError(f"Directory does not exist: {img_path}")
+    try: 
+        if os.environ["GITHUB_WORKSPACE"] != None:
+            root = os.environ["GITHUB_WORKSPACE"]
+            inPath = os.path.join(root, "TestGraphs", "Outputs")
+            
+    except: 
+        rootfrags = os.path.abspath('WS2.py').split("\\")
+        root = ""
+        for d in rootfrags:
+            if d == "FieldNBalance":
+                break
+            else:
+                root += d + "\\"
+        inPath = os.path.join(root,"FieldNBalance","TestGraphs", "Outputs")   
     
-    imgs = glob(osp.join(img_path, "*.png"))
+    if not osp.isdir(inPath):
+        raise FileNotFoundError(f"Directory does not exist: {inPath}")
+    
+    imgs = glob(osp.join(inPath, "*.png"))
     if len(imgs) == 0:
-        raise FileNotFoundError(f"No images found in directory: {img_path}")
+        raise FileNotFoundError(f"No images found in directory: {inPath}")
     
     doc = aw.Document()
     builder = aw.DocumentBuilder(doc)
@@ -24,3 +55,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+# -
+
+
